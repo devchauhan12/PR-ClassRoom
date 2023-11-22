0b15fb67bfc3434aa4a863a9a40e96103e2f5b89
vikas6938 <vikasborse000@email.com>
2023-11-22 12:57:38 +0530

    PR-Timer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Timer from './PR-Timer/Timer/Components/Timer';
+
+
+function App() {
+  return (
+    <>
+      <Timer />
+    </>
+  );
+}
+
+export default App;
