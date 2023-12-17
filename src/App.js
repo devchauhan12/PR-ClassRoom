70400a31ccd7a2a44641362f03ac5eec25f53b1c
Dev chauhan <devchauhansurat@gmail.com>
2023-12-18 02:47:34 +0530

    " PR-Prouting "

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
-import LocalStorage from './PR-LocalStorage/Components/LocalStorage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './PR-Routing/Components/Home';
+import Header from './PR-Routing/Components/Header';
+import About from './PR-Routing/Components/About';
+import AddUser from './PR-Routing/Components/AddUser';
+import { useEffect, useState } from 'react';
+import UserList from './PR-Routing/Components/UserList';
+
 
 
 function App() {
+  const [data, setData] = useState([])
+  const [state, setState] = useState(true)
+  useEffect(() => {
+    const localUsers = JSON.parse(localStorage.getItem('users'))
+    if (localUsers && localUsers.length > 0) {
+      setData(localUsers)
+    } else {
+      localStorage.setItem('users', JSON.stringify([]))
+    }
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('users', JSON.stringify(data))
+  }, [data])
   return (
-    <>
-      <LocalStorage />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/addUser/:id' element={<AddUser data={data} setData={setData} state={state} setState={setState} />} />
+        <Route path='/User-List' element={<UserList data={data} setData={setData} state={state} setState={setState} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
